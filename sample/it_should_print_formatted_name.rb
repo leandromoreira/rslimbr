d07c61733f9dba8386cce076cba8903a584fc0c4
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-14 17:16:50 -0300

    starting playing with spec and slim protocol itself

---
+++ sample/it_should_print_formatted_name.rb
@@ -0,0 +1,9 @@
+require 'person'
+
+class ItShouldPrintFormattedName
+	attr_accessor :name, :surname
+
+	def formatted_name
+		"#{@name} #{@surname}"
+	end
+end
---
+++ spec/command_line_spec.rb
@@ -0,0 +1,6 @@
+#according with http://fitnesse.org/FitNesse.UserGuide.SliM.SlimProtocol.PortManagement at 14 July 2011
+
+describe CommandLine do
+	it "should use the port 8085 if no command line argument was passed" do
+	end
+end
