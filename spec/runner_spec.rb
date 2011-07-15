45019cb554c44336c7e3797146c77a180024b1b1
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-15 16:43:30 -0300

    better messages on specs

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -2,25 +2,25 @@
 require File.dirname(__FILE__) + '/../lib/runner'
 
 describe Runner do
-	describe "the default port" do
-		it "should use the port 8085 if no command line argument was passed" do
+	describe "Default port" do
+		it "should use the port 8085 when no command line is pass" do
 		  runner = Runner.new(nil)
 			runner.port.should == 8085
 		end
 	end
-	describe "a passed port" do
-		it "should use the plain port passed on argument" do
+	describe "Plain port by argument" do
+		it "should use the plain port" do
 			runner = Runner.new(1515)
 			runner.port.should == 1515
 		end
 	end
-	describe "a passed port by -DSLIM_PORT=xxxx" do
-		it "should use the coded in parameter port on argument" do
+	describe "Coded port by argument -DSLIM_PORT=xxxx" do
+		it "should use the value of DSLIM_PORT" do
 			runner = Runner.new(" -DSLIM_PORT=2002 ")
 			runner.port.should == 2002			
 		end
 	end
-	describe "a malformed argument m@lform3D should be ignored" do
+	describe "Undefined parameter to runner" do
 		it "should use the default port 8085" do
 			runner = Runner.new(" m@lform3D ")
 			runner.port.should == 8085			
