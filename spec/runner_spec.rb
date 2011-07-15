80e9cbdcbc110e2165e3d40d6e98357277767b3a
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-15 13:43:19 -0300

    fixing the import for spec

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,11 +1,17 @@
 #http://fitnesse.org/FitNesse.UserGuide.SliM.SlimProtocol.PortManagement
-require '../lib/runner'
+require File.dirname(__FILE__) + '/../lib/runner'
 
 describe Runner do
 	describe "default port" do
 		it "should use the port 8085 if no command line argument was passed" do
 		  runner = Runner.new(nil)
 	    runner.port == 8085
+		end
+	end
+	describe "a passed port" do
+		it "should use the port passed on argument" do
+			runner = Runner.new(1515)
+			runner.port == 1515
 		end
 	end
 end
