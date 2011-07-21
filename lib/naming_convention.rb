a205e91205035434c4d7364c930bd94a4c1d243a
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2011-07-21 01:00:57 -0300

    Naming Convention module & spec

--- lib/naming_convention.rb
+++ lib/naming_convention.rb
@@ -1,6 +1,22 @@
-class NamingConvention
-	def set_property_name
-	end
-	def get_property_name
-	end
+class String
+    def to_snake_case
+        self.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+            gsub(/([a-z\d])([A-Z])/,'\1_\2').
+            downcase
+    end
+end
+module NamingConvention
+    def to_ruby(string)
+        raise Error.new("Can't convert null") if string.nil?
+        snaked = string.to_snake_case
+        if snaked.length > 3
+            case snaked[0..3]
+            when 'set_'
+                return snaked[4..-1] + '='
+            when 'get_'
+                return snaked[4..-1]
+            end
+        end
+        snaked
+    end
 end
--- spec/naming_convetion_spec.rb
+++ spec/naming_convetion_spec.rb
@@ -1,15 +1,28 @@
 #specs from http://fitnesse.org/FitNesse.UserGuide.SliM.SlimProtocol
 require File.dirname(__FILE__) + '/../lib/naming_convention'
 
-describe SlimRequest do
-	describe "Method naming convention" do
-		it "given the read property 'name' should return 'name'" do
-		end
-		it "given the read property 'formattedName' should return 'formatted_name'" do
-		end
-		it "given the write property 'setSurname' should return 'surname'" do
-		end
-		it "given the write property 'setUserName' should return 'user_name'" do
-		end
-	end
+describe NamingConvention do
+    include NamingConvention
+    describe "Method naming convention" do
+        it "given the read property 'name' should return 'name'" do
+            test = to_ruby('name')
+            test.should == 'name'
+        end
+        it "given the read property 'formattedName' should return 'formatted_name'" do
+            test = to_ruby('formattedName')
+            test.should == 'formatted_name'
+        end
+        it "given the write property 'setSurname' should return 'surname='" do
+            test = to_ruby('setSurname')
+            test.should == 'surname='
+        end
+        it "given the write property 'setUserName' should return 'user_name='" do
+            test = to_ruby('setUserName')
+            test.should == 'user_name='
+        end
+        it "given the read property 'getUserName' should return 'user_name'" do
+            test = to_ruby('getUserName')
+            test.should == 'user_name'
+        end
+    end
 end
