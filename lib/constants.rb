186e0edbd3fda087dade519c9f79545772fa0c12
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-22 18:24:14 -0300

    in fact we are work in progress, neither versions

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,6 +1,6 @@
 module Constants
 	DefaultPort = 8085
-	SlimVersion = "Slim -- V0.2\n"
+	SlimVersion = "Slim -- V0.0\n"
 	ExceptionMessage = "__EXCEPTION__:"
 	ByeMessageSize = 3
 	MessageLenghtOffset = 7
