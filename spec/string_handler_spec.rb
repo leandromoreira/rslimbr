51d5ff3e1dc3325c41ccd12ab4c79caa82a1040b
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2011-07-22 21:12:30 -0300

    Fixing test description

--- spec/string_handler_spec.rb
+++ spec/string_handler_spec.rb
@@ -4,7 +4,7 @@ describe StringHandler do
     before do
         @long_string = "This is a extremely long string just to test the correct zero padding in the final string. Does this string have a length of more than 3 digits? Probably not, so I'll just type a little more. Maybe I'll write you a poem. Roses are red, violets are blue. Osame Bin Laden is dead. Great. I think that'll do."
     end
-    describe "Slim string handling" do
+    describe "Slim string coding" do
         it "should put the 000000: before the string" do
             StringHandler.code('test').should == '000004:test'
             StringHandler.code(@long_string).should == "000305:#{@long_string}"
