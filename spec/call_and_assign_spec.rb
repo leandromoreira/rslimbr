fa4f185813c1f6178323290de3c6f1cbb7d5aaa8
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-24 02:25:00 -0300

    starting the call and assign instruction

---
+++ spec/call_and_assign_spec.rb
@@ -0,0 +1,13 @@
+#specs from http://fitnesse.org/FitNesse.UserGuide.SliM.SlimProtocol
+
+describe "Call and assign" do
+	pending "I really need to sleep"
+end
+
+
+=begin
+CallAndAssign: [<id>, callAndAssign, <symbol>, <instance>, <function>, <arg>...] 
+This instruction is identical to call except that the return value is saved in a symbol named <symbol>. 
+That symbol is kept in a dictionary in the SLIM system. 
+Symbol names may only contain letters
+=end
