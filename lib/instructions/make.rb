8d73dd4f93864dca4fcdf36e3f3139c95f3ad2d4
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-07-24 02:12:10 -0300

    commit message out! I'm still not happy with that code on Make... it's smelling duplicate code

--- lib/instructions/make.rb
+++ lib/instructions/make.rb
@@ -1,8 +1,6 @@
 require 'constants'
-require 'logging'
 
 class Make
-		include Logging
 		attr_accessor :instances
 		def initialize(imports=nil)
 			@instances = {}
@@ -26,23 +24,17 @@ class Make
 		end
 		private
 		def create_using_imports(instance,clazz,args=nil)
-			log "trying to create using imports"
 			@imports.each do |import|
 				begin
 					if args.nil?
-						log "args is nil"
-						log "trying to create #{import}.#{clazz}"
 						@instances[instance] = Object.const_get("#{import}.#{clazz}").new
 						return
 					else
-						log "args is not nil"
-						log "trying to create #{import}.#{clazz}"
 						@instances[instance] = Object.const_get("#{import}.#{clazz}").new(*args)
 						return
 					end
 				rescue NameError
 				end
 			end
-			log "none of objects were created"			
 		end
 end
